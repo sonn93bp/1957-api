c9e444d9018d5227f1ac89c8f5d03fa4b2ccc202
1957.son <1957.son@gmail.com>
2024-08-15 23:52:41 +0700

    add status for about

--- src/models/about.model.js
+++ src/models/about.model.js
@@ -1,15 +1,17 @@
 const { Schema, model } = require("mongoose");
 const { HidenJsonField } = require("../../lib/mongoose.hiden.plugin");
 const { SeoSchema } = require("./sub/seo.model");
+const { Status } = require("../../lib/utils/enum/status.enum");
 
 var aboutSchema = new Schema({
   title: {
     type: String,
     require: true,
   },
-  slug: {
-    type: String,
-    require: true,
+  status: {
+    type: Number,
+    enum: Object.values(Status),
+    default: Status.Pending,
   },
   content: {
     type: String,
