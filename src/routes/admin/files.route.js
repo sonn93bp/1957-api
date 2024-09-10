d38f357e80a9c2a7c4dffdf4d37a26cb7f477f23
1957 api <1957api@info.com>
2024-09-10 07:35:46 -0700

    change upload file to disk

--- src/routes/admin/files.route.js
+++ src/routes/admin/files.route.js
@@ -1,9 +1,9 @@
 const app = require("express").Router();
 const controller = require("../../controllers/upload.controller");
-const { upload } = require("../../../lib/config/storage.config");
+const { uploadLocal } = require("../../../lib/config/storage.config");
 app
   .route("/upload")
-  .post(upload.array("files[]", 10), controller.uploadMultiFile);
+  .post(uploadLocal.array("files[]", 10), controller.uploadMultiFile);
 
 app.route("/delete/:uid").delete(controller.deleteFileById);
 module.exports = app;
